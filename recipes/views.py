4172ed60d85014f2770cea3243a01eed4631510a
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-12-16 15:16:02 -0300

    Configurando utils factory.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+from utils.factory import make_recipe
+
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)]
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe.html')
+    return render(request, 'recipes/pages/recipe.html', context={
+        'recip': make_recipe()
+    })
